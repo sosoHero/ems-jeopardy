1e54bd3171e054265ddca0b9fbcfb8f9aab5d577
Chris Shouts <cshouts.code@gmail.com>
2017-06-22 15:14:53 -0500

    Support an arbitrary number of columns and rows. Fix bug when clicking on label. Disable buttons after click instead of removing them. Color selected buttons based on correct answer. Remove unused files.

---
+++ public/js/questions.js
@@ -0,0 +1,177 @@
+var categories = [
+  {
+    "name": "HTML",
+    "questions": [
+      {
+        "prompt": "What is the expansion of HTML?",
+        "options": [ "Hyper Text Markup Language", "Holding Text Marker Language", "Hyperlink Text Management Language", "Holding Text Modal Language" ],
+        "correctAnswer": "Hyper Text Markup Language",
+        "cashPrize": 200
+      },
+      {
+        "prompt": "What is the current version of HTML?",
+        "options": [ "HTMLv5.1", "HTML5", "HTML2016", "HTMLC" ],
+        "correctAnswer": "HTML5",
+        "cashPrize": 400
+      },
+      {
+        "prompt": "What seperate language has largely replaced use of <style></style> elements?",
+        "options": [ "HTMLS Code", "Style Sheet Language", "CSS or Central Style Sheet", "CSS or Cascading Style Sheets" ],
+        "correctAnswer": "CSS or Cascading Style Sheets",
+        "cashPrize": 600
+      },
+      {
+        "prompt": "What new element type introduced in HTML5 is functionally the same as a Div?",
+        "options": [ "Semantic Tags", "PseudoDivs", "DivisionIDs", "Division Notation" ],
+        "correctAnswer": "Semantic Tags",
+        "cashPrize": 800
+      },
+      {
+        "prompt": "HTML is the principal source for attributes of Nodes in the DOM. Browsers do not usually allow cross-site actions but there is one major exception:",
+        "options": [ "Get Requests", "Ping Commands", "iFrames", "XSS " ],
+        "correctAnswer": "iFrames",
+        "cashPrize": 1000
+      }
+    ]
+  },
+  {
+    "name": "CSS",
+    "questions": [
+      {
+        "prompt": "What is the expansion of CSS?",
+        "options": [ "Cascading Style Sheets", "Central Style Server", "Control Set Styles", "C Safe Server" ],
+        "correctAnswer": "Cascading Style Sheets",
+        "cashPrize": 200
+      },
+      {
+        "prompt": "The \"<Span> </Span>\" has what kind of default property?",
+        "options": [ "set-length", "block", "timestamp", "inline" ],
+        "correctAnswer": "inline",
+        "cashPrize": 400
+      },
+      {
+        "prompt": "The * selector applies to what?",
+        "options": [ "Everything", "Wildcard elements", "Multiplication Functions", "Elements with the Id of *" ],
+        "correctAnswer": "Everything",
+        "cashPrize": 600
+      },
+      {
+        "prompt": "What feature recently introduced in CSS 3 addresses issues with \"floats\"?",
+        "options": [ "Center Flagpoints", "Flexbox", "Flexpoints", "FloatV2.0" ],
+        "correctAnswer": "Flexbox",
+        "cashPrize": 800
+      },
+      {
+        "prompt": "CSS is hard. What is one feature that makes it hard compared to other languages? ",
+        "options": [ "Lack of Case Examples", "No Error Output", "Inclusion of complex variable functions", "Lack of documentation" ],
+        "correctAnswer": "No Error Output",
+        "cashPrize": 1000
+      }
+    ]
+  },
+  {
+    "name": "JS",
+    "questions": [
+      {
+        "prompt": "Which is a coding language or library not relevant to modern Javascript?",
+        "options": [ "Java", "ECMAScript", "Ajax", "JQuery" ],
+        "correctAnswer": "Java",
+        "cashPrize": 200
+      },
+      {
+        "prompt": "Javascript is usually described as a \" \"-based language?",
+        "options": [ "Relational", "COBOL", "Object", "Directional" ],
+        "correctAnswer": "Object",
+        "cashPrize": 400
+      },
+      {
+        "prompt": "JSON is: ",
+        "options": [ "A Javascript engine", "A text format readable by Javascript", "A Javascript library", "A Javascript function" ],
+        "correctAnswer": "A text format readable by Javascript",
+        "cashPrize": 600
+      },
+      {
+        "prompt": "Angular JS is an example of:",
+        "options": [ "A JS Framework", "an open-source, cross-platform runtime environment for developing server-side web applications", "A JS library", "An AJAX library" ],
+        "correctAnswer": "A JS Framework",
+        "cashPrize": 800
+      },
+      {
+        "prompt": "Imagine one hundred Samurai are standing in a circle each holding a sword. The first samurai kills the samurai next to them (the second).  The third Samurai then kills the forth Samurai and this process of Samurai killing the Samurai directly next to them continues until there is only one Samurai left standing at the end. You write a JS program with these Samurai in an Array and use functions to determine which of the Samurai will be the last one alive. What JS functions are required to do this?",
+        "options": [ "For Loop, If Else Function, Array Shift, Array Splice", "Fizzbuzz, Else Statements", "Loop Functions with Calculator Integers", "Array Reduce" ],
+        "correctAnswer": "For Loop, If Else Function, Array Shift, Array Splice",
+        "cashPrize": 1000
+      }
+    ]
+  },
+  {
+    "name": "UNIX",
+    "questions": [
+      {
+        "prompt": "The Terminal or command line is an example of a?",
+        "options": [ "Interconnected Network", "Read-Eval-Print-Loop", "C++ Library Object", "Skynet" ],
+        "correctAnswer": "Read-Eval-Print-Loop",
+        "cashPrize": 200
+      },
+      {
+        "prompt": "UNIX was added relatively later into what popular propietary operating system?",
+        "options": [ "Windows 10", "Linux Mint", "Mac OS X", "Ubuntu" ],
+        "correctAnswer": "Mac OS X",
+        "cashPrize": 400
+      },
+      {
+        "prompt": "The command \"man x\" would?",
+        "options": [ "Bring up the task manager for x", "Modularize x", "Attempt to bring up the manual page for x", "Make x mandatory on startup" ],
+        "correctAnswer": "Attempt to bring up the manual page for x",
+        "cashPrize": 600
+      },
+      {
+        "prompt": "Someone on the internet tells you to enter a \"rm -rf\" command into your terminal. This would:",
+        "options": [ "Remake your filedomains", "Rename your file register", "Recursively rename all your files with the tag rm", "Irreparably damage your file system" ],
+        "correctAnswer": "Irreparably damage your file system",
+        "cashPrize": 800
+      },
+      {
+        "prompt": "The GREP command allows a user to:",
+        "options": [ "Quickly search files for specific items of text", "Grab-Read-Evaluate-Loop", "Gauge RAM event parameters", "Group related events in the processor" ],
+        "correctAnswer": "Quickly search files for specific items of text",
+        "cashPrize": 1000
+      }
+    ]
+  },
+  {
+    "name": "BONUS",
+    "questions": [
+      {
+        "prompt": "Use of \"Alert\" popup windows is often considered bad practice as browsers and users are hostile to pop-ups. What are the other two native Javascript popup functions?",
+        "options": [ "Warning, Confirm", "Confirm, Refresh", "Prompt, Confirm", "Prompt, Notice" ],
+        "correctAnswer": "Prompt, Confirm",
+        "cashPrize": 200
+      },
+      {
+        "prompt": "The man page for Git describes it as:",
+        "options": [ "That stupid content tracker", "A Content Version Tracker", "General-Index-Tracker", "A Revision Editor" ],
+        "correctAnswer": "That stupid content tracker",
+        "cashPrize": 400
+      },
+      {
+        "prompt": "What version of what coding language introduced the audio function contained in this page?",
+        "options": [ "ECMAScript", "MP3ML", "HTML5", "WavML" ],
+        "correctAnswer": "HTML5",
+        "cashPrize": 600
+      },
+      {
+        "prompt": "Which of these frameworks include support for TypeScript?",
+        "options": [ "AngularJS", "Angular", "JQuery", "Whitespace" ],
+        "correctAnswer": "Angular",
+        "cashPrize": 800
+      },
+      {
+        "prompt": "What is an early computer language famous for being designed by Government and Business without much input from computer scientists?",
+        "options": [ "MS Office", "Telnet", "COBOL", "C--" ],
+        "correctAnswer": "COBOL",
+        "cashPrize": 1000
+      }
+    ]
+  }
+]
